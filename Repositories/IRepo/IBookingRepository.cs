1a525f6635520e187ca85221f970a9df255cc685
votrongluan <115349873+votrongluan@users.noreply.github.com>
2024-07-08 13:37:30 +0700

    fix: online payment with booking multiple day

--- Repositories/IRepo/IBookingRepository.cs
+++ Repositories/IRepo/IBookingRepository.cs
@@ -4,6 +4,7 @@ namespace Repositories.IRepo;
 
 public interface IBookingRepository
 {
+    Booking GetBookingByIdNoInclude(int bookingId);
     List<Booking> GetAllBookings();
     Booking GetBookingById(int bookingId);
     void DeleteBooking(int bookingId);
